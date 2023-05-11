576bc19f11002ac95528efba6ebd18ed0da1f51a
Omachonu Prosper <omachonucodes@gmail.com>
2023-05-11 04:22:39 +0100

    BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Base model that defines all common attributes/methods for other classes.
+"""
+
+import uuid
+import datetime
+
+
+class BaseModel:
+    """Base model that defines all common attributes/methods for other classes.
+    """
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates the public instance attribute updated_at
+        with the current datetime
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+        model_dict = self.__dict__
+        model_dict['__class__'] = self.__class__.__name__
+        return model_dict
