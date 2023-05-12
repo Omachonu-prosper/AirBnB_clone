59a57a2c1644eb0f112b3129f638c03c6e48c81e
Omachonu Prosper <omachonucodes@gmail.com>
2023-05-12 03:23:38 +0100

    Store first object

---
+++ models/engine/file_storage.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""Serializes instances to a JSON file and deserializes
+JSON file to instances
+"""
+
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage():
+    """Serializes instances to a JSON file and deserializes
+    JSON file to instances
+    """
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """Returns the class attribute dictionary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)
+        """
+        objects = {}
+        for key, value in self.__objects.items():
+            objects[key] = value.to_dict()
+
+        with open(self.__file_path, 'w+') as f:
+            json.dump(objects, f)
+
+
+    def reload(self):
+        """Seserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)
+        """
+        objects = {}
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as f:
+                objects = json.load(f)
+
+            for key, value in objects.items():
+                self.__objects[key] = BaseModel(**value)
